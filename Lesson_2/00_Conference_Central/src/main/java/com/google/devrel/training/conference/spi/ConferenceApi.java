80dfe54f30cd8205e323cf05278746775670cd7d
twhorten <troy.whorten@wyo.gov>
2015-05-12 13:10:24 -0600

    Debugged weird issue where ApiExplorer was hitting AppSpot instead of localhost... just had to update App Engine sdk to 1.9.19

--- Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -48,7 +48,7 @@ public class ConferenceApi {
 
     // TODO 1 Pass the ProfileForm parameter
     // TODO 2 Pass the User parameter
-    public Profile saveProfile() throws UnauthorizedException {
+    public Profile saveProfile(ProfileForm profileForm) throws UnauthorizedException {
 
         String userId = null;
         String mainEmail = null;
@@ -64,7 +64,11 @@ public class ConferenceApi {
 
         // TODO 1
         // Set the displayName to the value sent by the ProfileForm, if sent
-        // otherwise set it to null
+        displayName = profileForm.getDisplayName();
+        
+        if( profileForm.getTeeShirtSize() != null) {
+        	teeShirtSize = profileForm.getTeeShirtSize();
+        }
 
         // TODO 2
         // Get the userId and mainEmail
