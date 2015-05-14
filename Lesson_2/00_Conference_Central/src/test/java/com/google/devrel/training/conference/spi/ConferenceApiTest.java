91e24b048f604dbdb72447dc4819ebe7726abcd7
twhorten <troy.whorten@wyo.gov>
2015-05-14 15:58:16 -0600

    Added unit tests for Lesson 4 and skeleton code for Lesson 5

--- Lesson_2/00_Conference_Central/src/test/java/com/google/devrel/training/conference/spi/ConferenceApiTest.java
+++ Lesson_2/00_Conference_Central/src/test/java/com/google/devrel/training/conference/spi/ConferenceApiTest.java
@@ -7,8 +7,10 @@ import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.devrel.training.conference.domain.Conference;
 // import com.google.devrel.training.conference.domain.Conference;
 import com.google.devrel.training.conference.domain.Profile;
+import com.google.devrel.training.conference.form.ConferenceForm;
 // import com.google.devrel.training.conference.form.ConferenceForm;
 import com.google.devrel.training.conference.form.ProfileForm;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
@@ -169,7 +171,7 @@ public class ConferenceApiTest {
     }
 
 
-    /*
+    
     @Test
     public void testCreateConference() throws Exception {
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
@@ -186,7 +188,7 @@ public class ConferenceApiTest {
         assertEquals(NAME, conference.getName());
         assertEquals(DESCRIPTION, conference.getDescription());
         assertEquals(topics, conference.getTopics());
-        assertEquals(USER_ID, conference.getOrganizerGplusId());
+        assertEquals(USER_ID, conference.getOrganizerUserId());
         assertEquals(CITY, conference.getCity());
         assertEquals(startDate, conference.getStartDate());
         assertEquals(endDate, conference.getEndDate());
@@ -201,9 +203,7 @@ public class ConferenceApiTest {
         String displayName = EMAIL.substring(0, EMAIL.indexOf("@"));
         assertEquals(displayName, profile.getDisplayName());
     }
-    */
-
-    /*
+    
     @Test
     public void testGetConferencesCreated() throws Exception {
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
@@ -222,9 +222,7 @@ public class ConferenceApiTest {
         assertTrue("The result should contain a conference",
                 conferencesCreated.contains(conference));
     }
-    */
-
-    /*
+    
     @Test
     public void testGetConference() throws Exception {
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
@@ -242,7 +240,7 @@ public class ConferenceApiTest {
         assertEquals(NAME, conference.getName());
         assertEquals(DESCRIPTION, conference.getDescription());
         assertEquals(topics, conference.getTopics());
-        assertEquals(USER_ID, conference.getOrganizerGplusId());
+        assertEquals(USER_ID, conference.getOrganizerUserId());
         assertEquals(CITY, conference.getCity());
         assertEquals(startDate, conference.getStartDate());
         assertEquals(endDate, conference.getEndDate());
@@ -250,9 +248,7 @@ public class ConferenceApiTest {
         assertEquals(CAP, conference.getSeatsAvailable());
         assertEquals(MONTH, conference.getMonth());
     }
-    */
-
-    /*
+    
     @Test
     public void testRegistrations() throws Exception {
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
@@ -287,5 +283,5 @@ public class ConferenceApiTest {
         assertFalse("Profile shouldn't have the conferenceId in conferenceIdsToAttend.",
                 profile.getConferenceKeysToAttend().contains(conference.getWebsafeKey()));
     }
-    */
+    
 }
